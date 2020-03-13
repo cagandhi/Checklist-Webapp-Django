53f2ee3faf181768a0aac6bf7cf89ee9eb5b45b0
Chintan Gandhi <chintangandhi97caa@gmail.com>
2020-03-13 00:34:14 -0400

    part 6-user registration, create register form

--- checklist_project/urls.py
+++ checklist_project/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 # from checklist import views
+from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('checklist.urls')),
-    # path('', views.home),
+    path('register/', user_views.register, name='register')
 ]
