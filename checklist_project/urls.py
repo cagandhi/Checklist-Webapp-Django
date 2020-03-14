554ea30eb46b3d4d29cbc9fc7a751855726e8a99
Chintan Gandhi <chintangandhi97caa@gmail.com>
2020-03-14 16:13:49 -0400

    part 7-login and logout

--- checklist_project/urls.py
+++ checklist_project/urls.py
@@ -17,9 +17,13 @@ from django.contrib import admin
 from django.urls import path, include
 # from checklist import views
 from users import views as user_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('checklist.urls')),
-    path('register/', user_views.register, name='register')
+    path('register/', user_views.register, name='register'),
+    path('profile/', user_views.profile, name='profile'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout')
 ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegisterForm
-
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def register(request):
 	if request.method == 'POST':
@@ -9,9 +9,14 @@ def register(request):
 		if form.is_valid():
 			form.save()
 			username = form.cleaned_data.get('username')
-			messages.success(request, f'Account created for {username}')
-			return redirect('checklist-home')
+			messages.success(request, f'Your account has been created successfully! You can now log in!')
+			return redirect('login')
 	else:
 		form = UserRegisterForm()
 
 	return render(request, 'users/register.html', {'form': form})
+
+# decorator - user must be logged in to access this page
+@login_required
+def profile(request):
+	return render(request, 'users/profile.html')
