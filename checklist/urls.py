fdcf5df41add6c111c05d6cb550fbdf50958ce4a
Chintan Gandhi <chintangandhi97caa@gmail.com>
2020-03-17 14:33:46 -0400

    part-9_1 list and detail view

--- checklist/urls.py
+++ checklist/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from .views import ChecklistListView, ChecklistDetailView
 
 urlpatterns = [
-    path('', views.home, name='checklist-home'),
+    path('', ChecklistListView.as_view(), name='checklist-home'), #path('', views.home, name='checklist-home'),
+    path('checklist/<int:pk>/', ChecklistDetailView.as_view(), name='checklist-detail'),
     path('about/', views.about, name='checklist-about'),
     path('mychecklist/', views.mychecklist, name='checklist-mychecklist')
 ]
--- checklist/views.py
+++ checklist/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Checklist
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
 
@@ -9,10 +10,19 @@ def home(request):
 	# return HttpResponse('<h1>This is your home page! Welcome</h1>')
 
 	context = {
-		'checklists_var': Checklist.objects.all(),
+		'checklists_var': Checklist.objects.all().order_by('-date_posted'), # order by to order posts in descending order of date posted
 		'title': 'HOME'
 	}
 	return render(request, 'checklist/home.html', context) # because render looks in templates subdirectory, by default
+
+class ChecklistListView(ListView):
+	model = Checklist
+	template_name = 'checklist/home.html' # <app_name>/<model>_<viewtype>.html
+	context_object_name = 'checklists_var'
+	ordering = ['-date_posted']
+
+class ChecklistDetailView(DetailView):
+	model = Checklist
 
 def about(request):
 	return render(request, 'checklist/about.html')
