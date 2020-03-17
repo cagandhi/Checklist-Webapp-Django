bbcc03b0934ed6d31fe3de410307b31781f3d469
Chintan Gandhi <chintangandhi97caa@gmail.com>
2020-03-17 00:38:53 -0400

    part 9-update user profile, remove old pics

--- users/views.py
+++ users/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib.auth.decorators import login_required
+
+import os
+
 # Create your views here.
 def register(request):
 	if request.method == 'POST':
@@ -16,7 +19,54 @@ def register(request):
 
 	return render(request, 'users/register.html', {'form': form})
 
-# decorator - user must be logged in to access this page
+# "@" denotes a decorator - user must be logged in to access this page
 @login_required
 def profile(request):
-	return render(request, 'users/profile.html')
+	if request.method == 'POST':
+		u_form = UserUpdateForm(request.POST, instance=request.user)
+		p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+	else:
+		u_form = UserUpdateForm(instance=request.user)
+		p_form = ProfileUpdateForm(instance=request.user.profile)
+	
+	old_image_url = request.user.profile.image.url
+
+	if u_form.is_valid() and p_form.is_valid():
+		print('OLD IMAGE')
+		BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+		print(BASE_DIR)
+		print(old_image_url)
+		print('OLD IMAGE')
+		
+		"""
+		print('-----------------------------------------------------------------')
+		print('NEW IMAGE')
+		print(BASE_DIR)
+		print(request.user.profile.image.url)
+		print('NEW IMAGE')
+		"""
+
+		u_form.save()
+		p_form.save()
+
+		try:
+			# don't remove default image
+			if old_image_url.split('/')[-1] != 'default.jpg':
+				
+				# remove old image so that unused profile pics don't clutter space
+				os.remove(BASE_DIR+old_image_url)
+				print('OLD IMAGE REMOVED SUCCESSFULLY')
+
+		# if image does not exist		
+		except OSError as error:
+			print('OLD IMAGE NOT REMOVED')
+
+		messages.success(request, f'Your profile has been updated successfully!')
+		return redirect('profile')
+
+	context = {
+		'u_form': u_form, 
+		'p_form': p_form
+	}
+
+	return render(request, 'users/profile.html', context)
