29fd0506f7f748356f73c24cce5688a336abf5fc
cagandhi <cagandhi97@gmail.com>
2020-05-22 23:09:54 -0400

    added category table and foreign key in checklist table

--- checklist/models.py
+++ checklist/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 from djrichtextfield.models import RichTextField
 
-# Create your models here.
+
 class Checklist(models.Model):
 	title = models.CharField(max_length=100)
 	# content = models.TextField()
@@ -14,12 +14,14 @@ class Checklist(models.Model):
 	date_posted = models.DateTimeField(default=timezone.now)
 	author = models.ForeignKey(User, on_delete = models.CASCADE)
 	visibility = models.IntegerField(default=0)
+	category = models.ForeignKey('Category', null=True, on_delete = models.SET_NULL)
 
 	def __str__(self):
 		return self.title
 
 	def get_absolute_url(self):
 		return reverse('checklist-detail', kwargs={'pk': self.id})
+
 
 class Upvote(models.Model):
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -28,9 +30,17 @@ class Upvote(models.Model):
 	def __str__(self):
 		return self.user.username + " - " + self.checklist.title
 
+
 class Bookmark(models.Model):
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
 	checklist = models.ForeignKey(Checklist, on_delete=models.CASCADE)
 
 	def __str__(self):
 		return self.user.username + " - " + self.checklist.title
+
+
+class Category(models.Model):
+	name = models.CharField(max_length=100)
+
+	def __str__(self):
+		return self.name
