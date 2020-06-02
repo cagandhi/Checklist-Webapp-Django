35888003ad3499885f0c07c9c96993b2b1b7c93c
cagandhi <cagandhi97@gmail.com>
2020-06-02 15:19:33 -0400

    option to password change from profile page added

--- checklist_project/urls.py
+++ checklist_project/urls.py
@@ -22,6 +22,7 @@ from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
 
+# password change - https://docs.djangoproject.com/en/3.0/topics/auth/default/#using-the-views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('checklist.urls')),
@@ -30,18 +31,12 @@ urlpatterns = [
     path('profile/', user_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    path('password-reset/', 
-        auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), 
-        name='password_reset'),
-    path('password-reset/done', 
-        auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), 
-        name='password_reset_done'),
-    path('password-reset-confirm/<uidb64>/<token>/', 
-        auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), 
-        name='password_reset_confirm'),
-    path('password-reset-complete/', 
-        auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), 
-        name='password_reset_complete'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name='password_reset'),
+    path('password-reset/done', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'),
+    path('password-change/', auth_views.PasswordChangeView.as_view(template_name='users/password_change.html'), name='password_change'),
+    path('password-change/done', auth_views.PasswordChangeDoneView.as_view(template_name='users/password_change_done.html'), name='password_change_done'),
     path('djrichtextfield/', include('djrichtextfield.urls')),
 ]
 
--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import PasswordChangeDoneView
 
 import os
 
