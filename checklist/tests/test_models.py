b322adb8a91159c52f4a64553fc0ab4a22ab3183
Chintan Gandhi <cagandhi97@gmail.com>
2021-01-08 02:02:22 +0530

    add unit tests for bookmark and category models

--- checklist/tests/test_models.py
+++ checklist/tests/test_models.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from checklist.models import Checklist, Category, Item, Upvote
+from checklist.models import Checklist, Category, Item, Upvote, Bookmark, Follow
 from django.contrib.auth.models import User
 
 
@@ -91,4 +91,46 @@ class UpvoteModelTest(TestCase):
         )
 
     def tearDown(self):
-        User.objects.filter(id=ChecklistModelTest.user.id).delete()
+        User.objects.filter(id=BookmarkModelTest.user.id).delete()
+
+
+class BookmarkModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create_user(
+            username="testuser", password="12345"
+        )
+        cls.user.save()
+
+        cls.category = Category.objects.create(name="Category_test")
+        cls.checklist = Checklist.objects.create(
+            title="New Checklist 1",
+            content="Test content",
+            author=cls.user,
+            category=cls.category,
+        )
+
+        cls.bookmark = Bookmark.objects.create(
+            user=cls.user, checklist=cls.checklist
+        )
+
+    def test_str_method(self):
+        self.assertEqual(
+            BookmarkModelTest.bookmark.__str__(),
+            BookmarkModelTest.user.username + " - " + BookmarkModelTest.checklist.title,
+        )
+
+    def tearDown(self):
+        User.objects.filter(id=BookmarkModelTest.user.id).delete()
+
+
+class CategoryModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.category = Category.objects.create(name="Category_test")
+
+    def test_str_method(self):
+        self.assertEqual(
+            CategoryModelTest.category.__str__(),
+            CategoryModelTest.category.name
+        )
