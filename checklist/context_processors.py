2015bc1be68298f9b4e6cddc61d3dbaffcd8a8ad
Chintan Gandhi <cagandhi97@gmail.com>
2021-02-18 14:54:32 -0500

    removed notif template tag, used existing context processor

--- checklist/context_processors.py
+++ checklist/context_processors.py
@@ -1,8 +1,17 @@
 # https://stackoverflow.com/a/34903331/6543250 - to pass data to "base.html"
 
-from checklist.models import Category
+from checklist.models import Category, Notification
+
 
 def add_variable_to_context(request):
-	return {
-		'category_list': Category.objects.all()
-	}
+	context={}
+	context['category_list'] = Category.objects.all()
+	
+	if request.user.is_authenticated:
+		context['notif_list'] = Notification.objects.filter(
+			toUser=request.user
+		).order_by("-date_notified")
+	else:
+		context['notif_list'] = []
+
+	return context
