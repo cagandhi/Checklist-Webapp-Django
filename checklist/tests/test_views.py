4d4ef9262854171f6baebf8761099b527bfb1ddc
Chintan Gandhi <cagandhi97@gmail.com>
2021-03-31 17:47:13 -0400

    add tests for item crud views #34

--- checklist/tests/test_views.py
+++ checklist/tests/test_views.py
@@ -1,20 +1,24 @@
-from checklist.models import Bookmark, Category, Checklist
+from django.contrib.auth.models import User
+from django.test import TestCase
+from django.urls import resolve, reverse
+
+from checklist.models import Bookmark, Category, Checklist, Item
 from checklist.views import (
     BookmarkChecklistListView,
+    CategoryChecklistListView,
     ChecklistCreateView,
     ChecklistDeleteView,
     ChecklistDetailView,
     ChecklistListView,
     ChecklistUpdateView,
+    SearchChecklistListView,
     UpvoteChecklistListView,
     UserChecklistListView,
     UserDraftChecklistListView,
-    SearchChecklistListView,
-    CategoryChecklistListView,
+    ItemCreateView,
+    ItemDetailView,
+    ItemUpdateView,
 )
-from django.contrib.auth.models import User
-from django.test import TestCase
-from django.urls import resolve, reverse
 
 
 # define helper functions
@@ -41,6 +45,10 @@ def create_checklist(title, content, user, category, is_draft=False):
 
 def create_bookmark_upvote(user, checklist):
     return Bookmark.objects.create(user=user, checklist=checklist)
+
+
+def create_item(title, checklist, completed=False):
+    return Item.objects.create(title=title, checklist=checklist)
 
 
 # test listviews and detailviews
@@ -244,7 +252,7 @@ class TestUserDraftChecklistListView(TestCase):
         self.user.delete()
 
 
-# test CRUD views
+# test checklist CRUD views
 class TestChecklistDetailView(TestCase):
     def setUp(self):
         self.user = create_user_if_not_exists("testuser", "12345")
@@ -554,10 +562,123 @@ class TestCategoryChecklistListView(TestCase):
             content="content 1",
             user=self.user,
             category=self.category,
-            is_draft=False,
         )
         response = self.client.get(self.url)
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.context["checklist_upvotes"].number, 1)
         self.assertEqual(response.context["checklist_upvotes"][0][0], list1)
+
+
+# test item CRUD views
+class TestItemCreateView(TestCase):
+    def setUp(self):
+        self.user = create_user_if_not_exists("testuser", "12345")
+        self.category = create_category_if_not_exists("test_category")
+        self.list1 = create_checklist(
+            title="list 1",
+            content="content 1",
+            user=self.user,
+            category=self.category,
+        )
+
+    def test_view_url(self):
+        url = resolve("/checklist/1/item/new/")
+        self.assertEqual(url.func.__name__, ItemCreateView.__name__)
+
+    def test_view_template_guest(self):
+        response = self.client.get(reverse("item-create", kwargs={"checklist_id": 1}))
+        self.assertEqual(response.status_code, 302)
+
+    def test_view_template_login(self):
+        self.client.login(username="testuser", password="12345")
+        response = self.client.get(reverse("item-create", kwargs={"checklist_id": 1}))
+        self.assertTemplateUsed(response, "checklist/item_form.html")
+
+    def test_create_item_guest(self):
+        item_data = {"title": "item 1", "checklist": self.list1}
+
+        response = self.client.post("/checklist/1/item/new/", data=item_data)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, "/checklist/1/")
+
+    def test_create_item_login(self):
+        self.client.login(username="testuser", password="12345")
+        item_data = {"title": "item 1", "checklist": self.list1}
+
+        response = self.client.post("/checklist/1/item/new/", data=item_data)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, "/checklist/item/1/view/")
+
+
+class TestItemDetailView(TestCase):
+    def setUp(self):
+        self.user = create_user_if_not_exists("testuser", "12345")
+        self.category = create_category_if_not_exists("test_category")
+
+        self.list1 = create_checklist(
+            title="list 1",
+            content="content 1",
+            user=self.user,
+            category=self.category,
+            is_draft=True,
+        )
+
+        self.item1 = create_item(title="item 1", checklist=self.list1)
+
+    def test_view_url(self):
+        url = resolve("/checklist/item/1/view/")
+        self.assertEqual(url.func.__name__, ItemDetailView.__name__)
+
+    def test_view_template(self):
+        response = self.client.get(reverse("item-detail", kwargs={"pk": 1}))
+        self.assertTemplateUsed(response, "checklist/item_detail.html")
+
+    def test_title(self):
+        response = self.client.get(reverse("item-detail", kwargs={"pk": 1}))
+        self.assertEqual(response.context["item"].title, "item 1")
+
+
+class TestItemUpdateView(TestCase):
+    def setUp(self):
+        self.user = create_user_if_not_exists("testuser", "12345")
+        self.category = create_category_if_not_exists("test_category")
+        self.list1 = create_checklist(
+            title="list 1",
+            content="content 1",
+            user=self.user,
+            category=self.category,
+            is_draft=False,
+        )
+
+        self.item1 = create_item(title="item 1", checklist=self.list1)
+
+    def test_view_url(self):
+        url = resolve("/checklist/item/1/update/")
+        self.assertEqual(url.func.__name__, ItemUpdateView.__name__)
+
+    def test_view_template_guest(self):
+        response = self.client.get(reverse("item-update", kwargs={"pk": 1}))
+        self.assertEqual(response.status_code, 302)
+
+    def test_view_template_login(self):
+        self.client.login(username="testuser", password="12345")
+        response = self.client.get(reverse("item-update", kwargs={"pk": 1}))
+        self.assertTemplateUsed(response, "checklist/item_form.html")
+
+    def test_update_item_guest(self):
+        item_data = {"title": "item 1", "checklist": self.list1}
+
+        response = self.client.post(
+            reverse("item-update", kwargs={"pk": 1}), data=item_data
+        )
+        self.assertEqual(response.status_code, 302)
+
+    def test_update_checklist_login(self):
+        self.client.login(username="testuser", password="12345")
+        item_data = {"title": "item 1", "checklist": self.list1}
+
+        response = self.client.post(
+            reverse("checklist-update", kwargs={"pk": 1}), data=item_data
+        )
+        self.assertEqual(response.status_code, 200)
