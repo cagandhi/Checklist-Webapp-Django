0a5a2e9dcafb140ae065ff6839d318b91c604e97
Chintan Gandhi <cagandhi97@gmail.com>
2021-03-31 16:49:08 -0400

    add tests for search and category list views #34

--- checklist/tests/test_views.py
+++ checklist/tests/test_views.py
@@ -1,9 +1,17 @@
 from checklist.models import Bookmark, Category, Checklist
-from checklist.views import (BookmarkChecklistListView, ChecklistCreateView,
-                             ChecklistDeleteView, ChecklistDetailView,
-                             ChecklistListView, ChecklistUpdateView,
-                             UpvoteChecklistListView, UserChecklistListView,
-                             UserDraftChecklistListView,)
+from checklist.views import (
+    BookmarkChecklistListView,
+    ChecklistCreateView,
+    ChecklistDeleteView,
+    ChecklistDetailView,
+    ChecklistListView,
+    ChecklistUpdateView,
+    UpvoteChecklistListView,
+    UserChecklistListView,
+    UserDraftChecklistListView,
+    SearchChecklistListView,
+    CategoryChecklistListView,
+)
 from django.contrib.auth.models import User
 from django.test import TestCase
 from django.urls import resolve, reverse
@@ -65,6 +73,7 @@ class TestChecklistListView(TestCase):
 
         response = self.client.get(reverse("checklist-home"))
         self.assertEqual(response.context["checklist_upvotes"].number, 1)
+        print(response.context["checklist_upvotes"][0])
         self.assertEqual(response.context["checklist_upvotes"][0][0], list1)
 
     def test_two_lists(self):
@@ -178,6 +187,8 @@ class TestUserDraftChecklistListView(TestCase):
         response = self.client.get(reverse("user-drafts"))
 
         self.assertEqual(response.status_code, 302)
+        # lambda expression required because without lambda, client.get() will be executed which will result in error even before we check assertRaises()
+        # https://stackoverflow.com/a/44269882/6543250
         self.assertRaises(TypeError, lambda: response.context["checklist_upvotes"])
 
     def test_one_list(self):
@@ -481,3 +492,72 @@ class TestUpvoteChecklistListView(TestCase):
         response = self.client.get(reverse("bookmarks"))
         self.assertEqual(response.context["checklist_upvotes"].number, 1)
         self.assertEqual(response.context["checklist_upvotes"][0][0], upvote1)
+
+
+class TestSearchChecklistListView(TestCase):
+    def setUp(self):
+        self.user = create_user_if_not_exists("testuser", "12345")
+        self.category = create_category_if_not_exists("test_category")
+        self.list1 = create_checklist(
+            title="list 1",
+            content="content 1",
+            user=self.user,
+            category=self.category,
+            is_draft=False,
+        )
+        self.client.login(username="testuser", password="12345")
+
+    def test_view_url(self):
+        url = resolve("/search/")
+        self.assertEqual(url.func.__name__, SearchChecklistListView.__name__)
+
+    def test_view_template_error(self):
+        # lambda expression required because without lambda, client.get() will be executed which will result in error even before we check assertRaises()
+        # https://stackoverflow.com/a/44269882/6543250
+        self.assertRaises(UnboundLocalError, lambda: self.client.get(reverse("search")))
+
+    def test_view_template_noerror(self):
+        # https://docs.djangoproject.com/en/3.1/topics/testing/tools/#django.test.Client.get
+        response = self.client.get(reverse("search"), {"q": "list"})
+        self.assertTemplateUsed(response, "checklist/search_checklists.html")
+
+    def test_search(self):
+        response = self.client.get(reverse("search"), {"q": "list"})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context["checklist_upvotes"].number, 1)
+        self.assertEqual(response.context["checklist_upvotes"][0][0], self.list1)
+
+
+class TestCategoryChecklistListView(TestCase):
+    def setUp(self):
+        self.user = create_user_if_not_exists("testuser", "12345")
+        self.category = create_category_if_not_exists("test_category")
+        self.url = reverse("category", kwargs={"category": "test_category"})
+
+    def test_view_url(self):
+        url = resolve("/checklist/test_category/")
+        self.assertEqual(url.func.__name__, CategoryChecklistListView.__name__)
+
+    def test_view_template(self):
+        response = self.client.get(self.url)
+        self.assertTemplateUsed(response, "checklist/category_checklists.html")
+
+    def test_no_checklists(self):
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(response.context["checklist_upvotes"], [])
+
+    def test_one_checklist(self):
+        list1 = create_checklist(
+            title="list 1",
+            content="content 1",
+            user=self.user,
+            category=self.category,
+            is_draft=False,
+        )
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context["checklist_upvotes"].number, 1)
+        self.assertEqual(response.context["checklist_upvotes"][0][0], list1)
