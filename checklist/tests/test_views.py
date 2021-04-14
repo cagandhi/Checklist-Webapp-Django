0dcbfaadfdf39ef88d7350407f736161ea2cead1
Chintan Gandhi <cagandhi@ncsu.edu>
2021-04-14 18:46:53 -0400

    add tests for comment crud views #34

--- checklist/tests/test_views.py
+++ checklist/tests/test_views.py
@@ -2,7 +2,9 @@ from django.contrib.auth.models import User
 from django.test import TestCase
 from django.urls import resolve, reverse
 
-from checklist.models import Bookmark, Category, Checklist, Item
+from django.contrib.messages import get_messages
+
+from checklist.models import Bookmark, Category, Checklist, Item, Comment
 from checklist.views import (
     BookmarkChecklistListView,
     CategoryChecklistListView,
@@ -11,13 +13,15 @@ from checklist.views import (
     ChecklistDetailView,
     ChecklistListView,
     ChecklistUpdateView,
+    ItemCreateView,
+    ItemDetailView,
+    ItemUpdateView,
     SearchChecklistListView,
     UpvoteChecklistListView,
     UserChecklistListView,
     UserDraftChecklistListView,
-    ItemCreateView,
-    ItemDetailView,
-    ItemUpdateView,
+    CommentUpdateView,
+    CommentDeleteView,
 )
 
 
@@ -49,6 +53,12 @@ def create_bookmark_upvote(user, checklist):
 
 def create_item(title, checklist, completed=False):
     return Item.objects.create(title=title, checklist=checklist)
+
+
+def create_comment(checklist, user, body, parent):
+    return Comment.objects.create(
+        checklist=checklist, user=user, body=body, parent=parent
+    )
 
 
 # test listviews and detailviews
@@ -682,3 +692,161 @@ class TestItemUpdateView(TestCase):
             reverse("checklist-update", kwargs={"pk": 1}), data=item_data
         )
         self.assertEqual(response.status_code, 200)
+
+
+# test comment CRUD views
+class TestCommentUpdateView(TestCase):
+    def setUp(self):
+        self.user = create_user_if_not_exists("testuser", "12345")
+        self.category = create_category_if_not_exists("test_category")
+        self.list1 = create_checklist(
+            title="list 1",
+            content="content 1",
+            user=self.user,
+            category=self.category,
+            is_draft=False,
+        )
+
+        self.comment_parent = create_comment(
+            checklist=self.list1, user=self.user, body="Test comment 1", parent=None
+        )
+
+    def test_view_url(self):
+        url = resolve("/comment/1/update/")
+        self.assertEqual(url.func.__name__, CommentUpdateView.__name__)
+
+    def test_view_template_guest(self):
+        response = self.client.get(reverse("comment-update", kwargs={"pk": 1}))
+        self.assertEqual(response.status_code, 302)
+
+    def test_view_template_login(self):
+        self.client.login(username="testuser", password="12345")
+        response = self.client.get(reverse("comment-update", kwargs={"pk": 1}))
+        self.assertTemplateUsed(response, "checklist/comment_form.html")
+
+    def test_update_comment_guest(self):
+        comment_data = {
+            "checklist": self.list1,
+            "user": self.user,
+            "body": "Update to comment 1",
+        }
+
+        response = self.client.post(
+            reverse("comment-update", kwargs={"pk": 1}), data=comment_data
+        )
+        self.assertEqual(response.status_code, 302)
+
+    def test_update_checklist_login(self):
+        self.client.login(username="testuser", password="12345")
+        comment_data = {
+            "checklist": self.list1,
+            "user": self.user,
+            "body": "Update to comment 1",
+        }
+
+        response = self.client.post(
+            reverse("comment-update", kwargs={"pk": 1}), data=comment_data
+        )
+        # because it redirects me to the edit form page
+        self.assertEqual(response.status_code, 302)
+
+
+class TestCommentDeleteView(TestCase):
+    def setUp(self):
+        self.user = create_user_if_not_exists("testuser", "12345")
+        self.category = create_category_if_not_exists("test_category")
+        self.list1 = create_checklist(
+            title="list 1",
+            content="content 1",
+            user=self.user,
+            category=self.category,
+            is_draft=False,
+        )
+
+        self.comment_parent = create_comment(
+            checklist=self.list1, user=self.user, body="Test comment 1", parent=None
+        )
+
+    def test_view_url(self):
+        url = resolve("/comment/1/delete/")
+        self.assertEqual(url.func.__name__, CommentDeleteView.__name__)
+
+    def test_view_template_guest(self):
+        response = self.client.get(reverse("comment-delete", kwargs={"pk": 1}))
+        self.assertEqual(response.status_code, 302)
+
+    def test_view_template_login(self):
+        self.client.login(username="testuser", password="12345")
+        response = self.client.get(reverse("comment-delete", kwargs={"pk": 1}))
+        self.assertTemplateUsed(response, "checklist/comment_confirm_delete.html")
+
+    def test_delete_comment_guest(self):
+        response = self.client.post(reverse("comment-delete", kwargs={"pk": 1}))
+        self.assertEqual(response.status_code, 302)
+
+    def test_delete_checklist_login(self):
+        self.client.login(username="testuser", password="12345")
+        response = self.client.post(reverse("comment-update", kwargs={"pk": 1}))
+        self.assertEqual(response.status_code, 200)
+
+
+# test view functions
+class TestAboutView(TestCase):
+    def test_view_url(self):
+        url = resolve("/about/")
+        self.assertEqual(url.func.__name__, "about")
+
+    def test_view_template(self):
+        response = self.client.get(reverse("checklist-about"))
+        self.assertTemplateUsed(response, "checklist/about.html")
+
+
+class TestUpvoteChecklistView(TestCase):
+    def setUp(self):
+        self.user = create_user_if_not_exists("testuser", "12345")
+        self.user2 = create_user_if_not_exists("testuser2", "12345")
+
+        self.category = create_category_if_not_exists("test_category")
+        self.list1 = create_checklist(
+            title="list 1",
+            content="content 1",
+            user=self.user,
+            category=self.category,
+            is_draft=False,
+        )
+
+    def test_view_url(self):
+        url = resolve("/checklist/1/upvote/")
+        self.assertEqual(url.func.__name__, "upvote_checklist")
+
+    def test_upvote_guest(self):
+        response = self.client.get(
+            reverse("checklist-upvote", kwargs={"checklist_id": 1})
+        )
+        self.assertEqual(response.status_code, 302)
+
+    def test_upvote_login_author(self):
+        self.client.login(username="testuser", password="12345")
+        response = self.client.get(
+            reverse("checklist-upvote", kwargs={"checklist_id": 1})
+        )
+
+        # refer https://stackoverflow.com/a/14909727/6543250
+        messages = list(get_messages(response.wsgi_request))
+        self.assertEqual(len(messages), 1)
+        self.assertEqual(
+            str(messages[0]), "Action Denied! You cannot upvote your own checklist!"
+        )
+        self.assertRedirects(response, "/", status_code=302)
+
+    def test_upvote_login_other(self):
+        self.client.login(username="testuser2", password="12345")
+        response = self.client.get(
+            reverse("checklist-upvote", kwargs={"checklist_id": 1})
+        )
+
+        # refer https://stackoverflow.com/a/14909727/6543250
+        messages = list(get_messages(response.wsgi_request))
+        self.assertEqual(len(messages), 1)
+        self.assertEqual(str(messages[0]), "Checklist upvoted!")
+        self.assertRedirects(response, "/", status_code=302)
